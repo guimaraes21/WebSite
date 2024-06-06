fa65b1e7d7d708bb680c1ae40f9c0e8ebe3dc9d9
Luiz Felipe <luizfelipesiqueira83@gmail.com>
2024-06-06 20:38:41 -0300

    to conseguindo alguma coisa

---
+++ app/app.js
@@ -0,0 +1,11 @@
+// sidenav-initialization
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+//   --
+
+// parallax-initialization
+$(document).ready(function () {
+    $('.parallax').parallax();
+});
+//   --
